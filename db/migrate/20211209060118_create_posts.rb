e7cc25704aec92f265a151179e84ae57f0aaefd4
hoarus <hoarus@gmail.com>
2021-12-09 16:02:35 +1000

    Generate Users & Posts scaffolds

---
+++ db/migrate/20211209060118_create_posts.rb
@@ -0,0 +1,14 @@
+class CreatePosts < ActiveRecord::Migration[6.1]
+  def change
+    drop_table :posts
+
+    create_table :posts do |t|
+      t.string :title
+      t.string :link
+      t.text :body
+      t.integer :user_id
+
+      t.timestamps
+    end
+  end
+end

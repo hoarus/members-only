1996ff3339a3945e0d111c47b4739f41519390a8
hoarus <hoarus@gmail.com>
2021-12-09 16:37:43 +1000

    Add basic User views

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  validates :password, length: { in: 6..16 }, presence: true
+  validates :username, length: { in: 4..12 }, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+
+  has_many :posts, dependent: :destroy
 end

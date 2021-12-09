d34cc8fcc09bfb056085663a3556412c4163c13b
hoarus <hoarus@gmail.com>
2021-12-09 17:13:14 +1000

    Initialise Devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   validates :password, length: { in: 6..16 }, presence: true
   validates :username, length: { in: 4..12 }, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true

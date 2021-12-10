20122d0c6eebf51ad66565973283459da2ff7c48
hoarus <hoarus@gmail.com>
2021-12-10 12:40:01 +1000

    Configured login parameter for sign-in

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,27 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :password, length: { in: 6..16 }, presence: true
-  validates :username, length: { in: 4..12 }, presence: true, uniqueness: true
+  validates :username, length: { in: 4..12 }, presence: true, uniqueness: { case_sensitive: false }
+  # only allow letter, number, underscore and punctuation.
+  validates_format_of :username, with: /^[a-zA-Z0-9_\.]*$/, :multiline => true
   validates :email, presence: true, uniqueness: true
 
   has_many :posts, dependent: :destroy
+
+
+  attr_writer :login
+
+  def login
+    @login || self.username || self.email
+  end
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions.to_h).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    elsif conditions.has_key?(:username) || conditions.has_key?(:email)
+      where(conditions.to_h).first
+    end
+  end
+  
 end

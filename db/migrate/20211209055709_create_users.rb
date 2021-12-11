e97154dcfd01257c72a0067747246f3b63f38114
hoarus <hoarus@gmail.com>
2021-12-12 07:17:22 +1000

    Remove non-required drop_tables from migrations

--- db/migrate/20211209055709_create_users.rb
+++ db/migrate/20211209055709_create_users.rb
@@ -1,7 +1,5 @@
 class CreateUsers < ActiveRecord::Migration[6.1]
   def change
-    drop_table :users 
-
     create_table :users do |t|
       t.string :username
       t.string :email
--- db/migrate/20211209060118_create_posts.rb
+++ db/migrate/20211209060118_create_posts.rb
@@ -1,7 +1,5 @@
 class CreatePosts < ActiveRecord::Migration[6.1]
   def change
-    drop_table :posts
-
     create_table :posts do |t|
       t.string :title
       t.string :link
